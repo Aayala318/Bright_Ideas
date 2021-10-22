bb3adfa8a8b9b830526472ce2584587371f0d994
Aayala318 <81716043+Aayala318@users.noreply.github.com>
2021-10-21 18:14:40 -0700

    Update settings.py

--- Bright_Ideas/settings.py
+++ Bright_Ideas/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')1y(npq0!lbb2ii#^kyxfejdr0sc-*fgvd9krovm+x3&#=+0y%'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
